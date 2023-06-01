60827a3c41ff98d3bdc542101310d486f8d808f5
Thiago2VP <thiagovasconcelosvp@protonmail.com>
2023-05-31 22:41:52 -0300

    home page complete

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as Styles from './styled';
 import chester from '../../assets/images/chester.png';
 import fandangos from '../../assets/images/fandangos.png';
 import mouth from '../../assets/images/ruffles.png';
 import respingo from '../../assets/images/respingo.png';
+import arrowRight from '../../assets/images/arrow-right.png';
+import naruLogo from '../../assets/images/narulogo.png';
+import wolverLogo from '../../assets/images/wolverlogo.png';
 
 export default function Home() {
   const [mediaW, setMediaW] = useState(window.innerWidth);
   const [mediaH, setMediaH] = useState(window.innerHeight);
+  const [preVis, setPreVis] = useState('hidden');
+  const [dracoW, setDracoW] = useState('0%');
+  const [dracVis, setDracVis] = useState('hidden');
+  const [mitoW, setMitoW] = useState('0%');
+  const [mitoVis, setMitoVis] = useState('hidden');
+  const [naruW, setNaruW] = useState('0%');
+  const [naruVis, setNaruVis] = useState('hidden');
+  const [xmenW, setXmenW] = useState('0%');
+  const [xmenVis, setXmenVis] = useState('hidden');
 
   if (mediaW > 1280) {
     setMediaW(1280);
@@ -22,6 +35,34 @@ export default function Home() {
   useEffect(() => {
     window.addEventListener('resize', handleResize);
   });
+
+  setTimeout(() => {
+    setPreVis('visible');
+  }, '600');
+
+  async function dracVisib() {
+    setTimeout(() => {
+      setDracVis('visible');
+    }, '1800');
+  }
+
+  async function mitVisib() {
+    setTimeout(() => {
+      setMitoVis('visible');
+    }, '1800');
+  }
+
+  async function narVisib() {
+    setTimeout(() => {
+      setNaruVis('visible');
+    }, '1800');
+  }
+
+  async function xmeVisib() {
+    setTimeout(() => {
+      setXmenVis('visible');
+    }, '1800');
+  }
 
   return (
     <main>
@@ -96,9 +137,365 @@ export default function Home() {
                   right: '-20%',
                 }}
               />
-              <h1>Cards Collection</h1>
-              <p>Veja algumas das coleções de cartas que vinham nos salgadinhos da elma chips</p>
+              <h1 style={{ visibility: `${preVis}` }}>Cards Collection</h1>
+              <p style={{ visibility: `${preVis}` }}>
+                Veja algumas das coleções de cartas que vinham nos salgadinhos da elma chips
+              </p>
+              <button
+                type="button"
+                className="arrowRghtMain"
+                style={{
+                  position: 'absolute',
+                  width: '13%',
+                  zIndex: '1',
+                  bottom: '6%',
+                  right: '8%',
+                  visibility: `${preVis}`,
+                  cursor: 'pointer',
+                  background: '#ddd21f',
+                  border: 'none',
+                }}
+                onClick={() => {
+                  setDracoW('100%');
+                  dracVisib();
+                }}
+                onKeyDown={() => {
+                  setDracoW('100%');
+                  dracVisib();
+                }}
+              >
+                <img style={{ width: '100%' }} src={arrowRight} alt="Seta para a direita" />
+              </button>
             </Styles.Elma>
+            <Styles.Dracomania style={{ width: `${dracoW}` }}>
+              <Link
+                to="/dracomania"
+                className="LogoDraco"
+                style={{
+                  position: 'absolute',
+                  top: '8%',
+                  right: '8%',
+                  textDecoration: 'none',
+                  fontSize: '6rem',
+                  fontFamily: 'ArgorBiwSc, sans-serif',
+                  letterSpacing: '1px',
+                  color: '#530303',
+                  textShadow: '1px 1px #bd0202',
+                  visibility: `${dracVis}`,
+                }}
+              >
+                ¤+Dracomania
+              </Link>
+              <button
+                type="button"
+                className="LogoDraco"
+                style={{
+                  position: 'absolute',
+                  zIndex: '1',
+                  bottom: '6%',
+                  left: '8%',
+                  width: '6%',
+                  height: '12%',
+                  paddingBottom: '2rem',
+                  visibility: `${dracVis}`,
+                  cursor: 'pointer',
+                  background: '#530303',
+                  border: 'none',
+                  borderRadius: '100%',
+                  color: '#2a300c',
+                  fontSize: '8rem',
+                  fontFamily: 'ArgorBiwSc, sans-serif',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onClick={() => {
+                  setDracoW('0%');
+                  setDracVis('hidden');
+                }}
+                onKeyDown={() => {
+                  setDracoW('0%');
+                  setDracVis('hidden');
+                }}
+              >
+                {'<'}
+              </button>
+              <button
+                type="button"
+                className="LogoDraco"
+                style={{
+                  position: 'absolute',
+                  zIndex: '1',
+                  bottom: '6%',
+                  right: '8%',
+                  width: '6%',
+                  height: '12%',
+                  paddingBottom: '2rem',
+                  visibility: `${dracVis}`,
+                  cursor: 'pointer',
+                  background: '#530303',
+                  border: 'none',
+                  borderRadius: '100%',
+                  color: '#2a300c',
+                  fontSize: '8rem',
+                  fontFamily: 'ArgorBiwSc, sans-serif',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onClick={() => {
+                  setMitoW('100%');
+                  mitVisib();
+                  setDracVis('hidden');
+                }}
+                onKeyDown={() => {
+                  setMitoW('100%');
+                  mitVisib();
+                  setDracVis('hidden');
+                }}
+              >
+                {'>'}
+              </button>
+            </Styles.Dracomania>
+            <Styles.Mitomania style={{ width: `${mitoW}` }}>
+              <Link
+                to="/mitomania"
+                className="LogoMito"
+                style={{
+                  position: 'absolute',
+                  top: '8%',
+                  left: '8%',
+                  textDecoration: 'none',
+                  fontSize: '6.5rem',
+                  fontFamily: 'Flatearth, sans-serif',
+                  fontWeight: 'bold',
+                  letterSpacing: '1px',
+                  color: '#743f22',
+                  textShadow: '1px 1px black',
+                  visibility: `${mitoVis}`,
+                }}
+              >
+                Mithomania
+              </Link>
+              <button
+                type="button"
+                className="LogoMito"
+                style={{
+                  position: 'absolute',
+                  zIndex: '1',
+                  bottom: '6%',
+                  left: '8%',
+                  width: '7%',
+                  height: '14%',
+                  paddingBottom: '1rem',
+                  visibility: `${mitoVis}`,
+                  cursor: 'pointer',
+                  background: '#ffd681',
+                  border: 'none',
+                  borderRadius: '100%',
+                  color: '#743f22',
+                  fontSize: '5.5rem',
+                  fontWeight: 'bold',
+                  fontFamily: 'Flatearth, sans-serif',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onClick={() => {
+                  setMitoW('0%');
+                  setMitoVis('hidden');
+                  dracVisib();
+                }}
+                onKeyDown={() => {
+                  setMitoW('0%');
+                  setMitoVis('hidden');
+                  dracVisib();
+                }}
+              >
+                ↞
+              </button>
+              <button
+                type="button"
+                className="LogoMito"
+                style={{
+                  position: 'absolute',
+                  zIndex: '1',
+                  bottom: '6%',
+                  right: '8%',
+                  width: '7%',
+                  height: '14%',
+                  paddingBottom: '1rem',
+                  visibility: `${mitoVis}`,
+                  cursor: 'pointer',
+                  background: '#ffd681',
+                  border: 'none',
+                  borderRadius: '100%',
+                  color: '#743f22',
+                  fontSize: '5.5rem',
+                  fontWeight: 'bold',
+                  fontFamily: 'Flatearth, sans-serif',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onClick={() => {
+                  setNaruW('100%');
+                  narVisib();
+                  setMitoVis('hidden');
+                }}
+                onKeyDown={() => {
+                  setNaruW('100%');
+                  narVisib();
+                  setMitoVis('hidden');
+                }}
+              >
+                ↠
+              </button>
+            </Styles.Mitomania>
+            <Styles.Naruto style={{ width: `${naruW}` }}>
+              <Link
+                to="/naruto"
+                style={{
+                  position: 'absolute',
+                  top: '8%',
+                  right: '8%',
+                  width: '23%',
+                  visibility: `${naruVis}`,
+                }}
+              >
+                <img src={naruLogo} alt="Logotipo do naruto" style={{ width: '100%' }} className="logoImg" />
+              </Link>
+              <button
+                type="button"
+                className="LogoNaru ArrowLeft"
+                style={{
+                  position: 'absolute',
+                  zIndex: '1',
+                  bottom: '6%',
+                  left: '8%',
+                  width: '6.5%',
+                  height: '13%',
+                  paddingBottom: '1rem',
+                  visibility: `${naruVis}`,
+                  cursor: 'pointer',
+                  background: '#a3545a',
+                  border: 'none',
+                  borderRadius: '100%',
+                  color: '#20120b',
+                  fontSize: '5.5rem',
+                  fontWeight: 'bold',
+                  fontFamily: 'Flatearth, sans-serif',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onClick={() => {
+                  setNaruW('0%');
+                  setNaruVis('hidden');
+                  mitVisib();
+                }}
+                onKeyDown={() => {
+                  setNaruW('0%');
+                  setNaruVis('hidden');
+                  mitVisib();
+                }}
+              >
+                ➡
+              </button>
+              <button
+                type="button"
+                className="LogoNaru"
+                style={{
+                  position: 'absolute',
+                  zIndex: '1',
+                  bottom: '6%',
+                  right: '8%',
+                  width: '6.5%',
+                  height: '13%',
+                  paddingBottom: '1rem',
+                  visibility: `${naruVis}`,
+                  cursor: 'pointer',
+                  background: '#a3545a',
+                  border: 'none',
+                  borderRadius: '100%',
+                  color: '#20120b',
+                  fontSize: '5.5rem',
+                  fontWeight: 'bold',
+                  fontFamily: 'Flatearth, sans-serif',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onClick={() => {
+                  setXmenW('100%');
+                  xmeVisib();
+                  setNaruVis('hidden');
+                }}
+                onKeyDown={() => {
+                  setXmenW('100%');
+                  xmeVisib();
+                  setNaruVis('hidden');
+                }}
+              >
+                ➡
+              </button>
+            </Styles.Naruto>
+            <Styles.Xmen style={{ width: `${xmenW}` }}>
+              <Link
+                to="/xmen"
+                style={{
+                  position: 'absolute',
+                  top: '8%',
+                  right: '8%',
+                  width: '19%',
+                  visibility: `${xmenVis}`,
+                }}
+              >
+                <img
+                  src={wolverLogo}
+                  alt="Logotipo de Wolverine e os X-men"
+                  style={{ width: '100%' }}
+                  className="logoImg"
+                />
+              </Link>
+              <button
+                type="button"
+                className="LogoXmen ArrowLeft"
+                style={{
+                  position: 'absolute',
+                  zIndex: '1',
+                  bottom: '6%',
+                  left: '8%',
+                  width: '6.5%',
+                  height: '13%',
+                  paddingBottom: '1rem',
+                  visibility: `${xmenVis}`,
+                  cursor: 'pointer',
+                  background: '#d1a01d',
+                  border: 'none',
+                  borderRadius: '100%',
+                  color: '#971312',
+                  fontSize: '5.5rem',
+                  fontWeight: 'bold',
+                  fontFamily: 'Flatearth, sans-serif',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onClick={() => {
+                  setXmenW('0%');
+                  setXmenVis('hidden');
+                  narVisib();
+                }}
+                onKeyDown={() => {
+                  setXmenW('0%');
+                  setXmenVis('hidden');
+                  narVisib();
+                }}
+              >
+                ➛
+              </button>
+            </Styles.Xmen>
           </Styles.Screen>
           <Styles.Camera>
             <Styles.CamRefl />
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 import * as colors from '../../config/colors';
+import draBack from '../../assets/images/dracomania.webp';
+import mitback from '../../assets/images/mitomania.webp';
+import narBack from '../../assets/images/naruto.webp';
+import wolback from '../../assets/images/wolverine.webp';
 
 export const SectionWide = styled.section`
   margin: auto;
@@ -22,6 +26,7 @@ export const Screen = styled.div`
   position: relative;
   display: flex;
   flex-direction: row;
+  overflow: hidden;
 `;
 
 export const Camera = styled.div`
@@ -119,6 +124,7 @@ export const Elma = styled.div`
     text-align: center;
     letter-spacing: 0.2rem;
     color: ${colors.color};
+    transition: visibility 0s opacity 0.5s linear;
   }
   p {
     text-align: center;
@@ -126,5 +132,194 @@ export const Elma = styled.div`
     font-weight: bold;
     max-width: 80%;
     margin: 1.5rem auto;
+    transition: visibility 0s opacity 0.5s linear;
+  }
+  .arrowRghtMain {
+    animation: pointing 1.2s linear 0.1s infinite;
+    img {
+      transform: rotate(-15deg);
+    }
+  }
+
+  @keyframes pointing {
+    0% {
+      margin-right: 0;
+    }
+    25% {
+      margin-right: 15px;
+    }
+    100% {
+      margin-right: 0;
+    }
+  }
+`;
+
+export const Dracomania = styled.div`
+  overflow: hidden;
+  background: green;
+  background-image: url(${draBack});
+  background-size: auto 720px;
+  background-position: top left;
+  display: flex;
+  flex-direction: column;
+  align-itens: center;
+  justify-content: center;
+  height: 100%;
+  border-radius: 3.5rem;
+  position: absolute;
+  right: 0;
+  z-index: 2;
+  transition: width 1.8s linear 0.1s;
+
+  .LogoDraco {
+    animation: shadowChange 1.5s linear 0.1s infinite;
+  }
+
+  @keyframes shadowChange {
+    0% {
+      text-shadow: 0px 0px;
+    }
+    50% {
+      text-shadow: 1px 1px;
+    }
+    100% {
+      text-shadow: 0px 0px;
+    }
+  }
+`;
+
+export const Mitomania = styled.div`
+  overflow: hidden;
+  background: gold;
+  background-image: url(${mitback});
+  background-size: auto 720px;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-itens: center;
+  justify-content: center;
+  height: 100%;
+  border-radius: 3.5rem;
+  position: absolute;
+  right: 0;
+  z-index: 2;
+  transition: width 1.8s linear 0.1s;
+
+  .LogoMito {
+    animation: shadowChange 1.5s linear 0.1s infinite;
+  }
+
+  @keyframes shadowChange {
+    0% {
+      text-shadow: 0px 0px;
+    }
+    50% {
+      text-shadow: 1px 1px;
+    }
+    100% {
+      text-shadow: 0px 0px;
+    }
+  }
+`;
+
+export const Naruto = styled.div`
+  overflow: hidden;
+  background: yellow;
+  background-image: url(${narBack});
+  background-size: auto 720px;
+  background-position: top right;
+  display: flex;
+  flex-direction: column;
+  align-itens: center;
+  justify-content: center;
+  height: 100%;
+  border-radius: 3.5rem;
+  position: absolute;
+  right: 0;
+  z-index: 2;
+  transition: width 1.8s linear 0.1s;
+
+  .LogoNaru {
+    animation: shadowChange 1.5s linear 0.1s infinite;
+  }
+  .logoImg {
+    animation: logoChange 1.5s linear 0.1s infinite;
+  }
+  .ArrowLeft {
+    transform: rotate(180deg);
+  }
+
+  @keyframes shadowChange {
+    0% {
+      text-shadow: 0px 0px;
+    }
+    50% {
+      text-shadow: -1px -1px;
+    }
+    100% {
+      text-shadow: 0px 0px;
+    }
+  }
+  @keyframes logoChange {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.05);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+`;
+
+export const Xmen = styled.div`
+  overflow: hidden;
+  background: red;
+  background-image: url(${wolback});
+  background-size: 1280px 720px;
+  background-position: top center;
+  display: flex;
+  flex-direction: column;
+  align-itens: center;
+  justify-content: center;
+  height: 100%;
+  border-radius: 3.5rem;
+  position: absolute;
+  right: 0;
+  z-index: 2;
+  transition: width 1.8s linear 0.1s;
+
+  .LogoXmen {
+    animation: shadowChange 1.5s linear 0.1s infinite;
+  }
+  .logoImg {
+    animation: logoChange 1.5s linear 0.1s infinite;
+  }
+  .ArrowLeft {
+    transform: rotate(180deg);
+  }
+
+  @keyframes shadowChange {
+    0% {
+      text-shadow: 0px 0px;
+    }
+    50% {
+      text-shadow: -1px -1px;
+    }
+    100% {
+      text-shadow: 0px 0px;
+    }
+  }
+  @keyframes logoChange {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.05);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 `;
